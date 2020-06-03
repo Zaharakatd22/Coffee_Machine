7ad28787ac9cd23a8097a1b5edc6678bdc68b5e3
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-03 11:05:06 +0400

    Add print count of possible cups of coffee

--- main.py
+++ main.py
@@ -6,15 +6,29 @@
 # print("Pouring some milk into the cup")
 # print("Coffee is ready!")
 
+WATERMACHIHE: int = 200
+MILKMACHIHE: int = 50
+BEANMACHINE: int = 15
 
-print("Write how many cups of coffee you will need:")
-cups_count = int(input("> "))
 
-water_count = cups_count * 200
-milk_count = cups_count * 50
-beans_count = cups_count * 15
+water_count: int = int(input("Write how many ml of water the coffee machine has: \n"
+                             "> "))
+milk_count: int = int(input("Write how many ml of milk the coffee machine has: \n"
+                            "> "))
+beans_count: int = int(input("Write how many grams of coffee beans the coffee machine has:: \n"
+                             "> "))
+cups_count: int = int(input("Write how many cups of coffee you will need:\n"
+                            "> "))
 
-print("For {} cups of coffee you will need: ".format(cups_count))
-print("{} ml of water".format(water_count))
-print("{} ml of milk".format(milk_count))
-print("{} g of coffee beans".format(beans_count))
+possible_water: int = water_count // WATERMACHIHE
+possible_milk: int = milk_count // MILKMACHIHE
+possible_beans: int = beans_count // BEANMACHINE
+
+possible_cups = min(possible_water, possible_milk, possible_beans)
+
+if cups_count == possible_cups:
+    print("Yes, I can make that amount of coffee")
+elif cups_count < possible_cups:
+    print(f"Yes, I can make that amount of coffee (and even {possible_cups - cups_count} more than that)")
+else:
+    print(f"No, I can make only {possible_cups} cups of coffee")
