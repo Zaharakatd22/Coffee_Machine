38f9f57da28898fa3ec2a798b3e7a9954d0ed5a7
Zaharakatd <37711267+Zaharakatd22@users.noreply.github.com>
2020-06-04 08:41:08 +0400

    Add skeleton of coffee machine functions and add constants of diffrent types of coffee

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # constants
-WATERMACHIHE: int = 200
-MILKMACHIHE: int = 50
-BEANMACHINE: int = 15
+WATER_ESPRESSO: int = 250
+MILK_ESPRESSO: int = 0
+BEAN_ESPRESSO: int = 16
+MONEY_ESPRESSO: int = 4
 
+WATER_LATTE: int = 250
+MILK_LATTE: int = 0
+BEAN_LATTE: int = 16
+MONEY_LATTE: int = 4
+
+WATER_CAPPUCCINO: int = 250
+MILK_CAPPUCCINO: int = 0
+BEAN_CAPPUCCINO: int = 16
+MONEY_CAPPUCCINO: int = 4
 
 # begin values
 begin_water: int = 400
@@ -26,27 +36,51 @@ def print_current_status(curr_water: int,
     print()
 
 
+def buy_coffee():
+    coffee_type: str = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: \n"
+                             "> ")
+
+    print_current_status(begin_water, begin_milk, begin_beans, begin_disposable_cups, begin_money)
+
+
+def fill_machine():
+    print_current_status(begin_water, begin_milk, begin_beans, begin_disposable_cups, begin_money)
+
+
+def take_money():
+    print_current_status(begin_water, begin_milk, begin_beans, begin_disposable_cups, begin_money)
+
+
 print_current_status(begin_water, begin_milk, begin_beans, begin_disposable_cups, begin_money)
 
+request: str = input("Write action (buy, fill, take): \n"
+                     "> ")
 
-water_count: int = int(input("Write how many ml of water the coffee machine has: \n"
-                             "> "))
-milk_count: int = int(input("Write how many ml of milk the coffee machine has: \n"
-                            "> "))
-beans_count: int = int(input("Write how many grams of coffee beans the coffee machine has:: \n"
-                             "> "))
-cups_count: int = int(input("Write how many cups of coffee you will need:\n"
-                            "> "))
+if request == "buy":
+    buy_coffee()
+elif request == "fill":
+    fill_machine()
+else:  # == "take"
+    take_money()
 
-possible_water: int = water_count // WATERMACHIHE
-possible_milk: int = milk_count // MILKMACHIHE
-possible_beans: int = beans_count // BEANMACHINE
-
-possible_cups = min(possible_water, possible_milk, possible_beans)
-
-if cups_count == possible_cups:
-    print("Yes, I can make that amount of coffee")
-elif cups_count < possible_cups:
-    print(f"Yes, I can make that amount of coffee (and even {possible_cups - cups_count} more than that)")
-else:
-    print(f"No, I can make only {possible_cups} cups of coffee")
+# water_count: int = int(input("Write how many ml of water the coffee machine has: \n"
+#                              "> "))
+# milk_count: int = int(input("Write how many ml of milk the coffee machine has: \n"
+#                             "> "))
+# beans_count: int = int(input("Write how many grams of coffee beans the coffee machine has:: \n"
+#                              "> "))
+# cups_count: int = int(input("Write how many cups of coffee you will need:\n"
+#                             "> "))
+#
+# possible_water: int = water_count // WATERMACHIHE
+# possible_milk: int = milk_count // MILKMACHIHE
+# possible_beans: int = beans_count // BEANMACHINE
+#
+# possible_cups = min(possible_water, possible_milk, possible_beans)
+#
+# if cups_count == possible_cups:
+#     print("Yes, I can make that amount of coffee")
+# elif cups_count < possible_cups:
+#     print(f"Yes, I can make that amount of coffee (and even {possible_cups - cups_count} more than that)")
+# else:
+#     print(f"No, I can make only {possible_cups} cups of coffee")
